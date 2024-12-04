cdc49b5ae2129e40de13c01d59faf191f0f2160c
Vitaliy Sobol <vitaliy.sobol-contractor@procore.com>
2024-12-04 17:54:37 +0200

    add ENV veeriable to ci; update a version

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 from src.db_writer import DatabaseWriter
 from src.log_reader import LogFileReader
 
-__version__ = "1.0.1"
+__version__ = "1.1.1"
 __author__ = "Vitaliy Sobol"
 
 __all__ = ["LogFileReader", "DatabaseWriter"]
